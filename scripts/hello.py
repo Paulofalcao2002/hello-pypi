18a93a64f6f5f20f7cc534df5388c2eb33162559
Paulo <paulohenriquemoreirafalcao@gmail.com>
2024-09-14 11:45:03 -0300

    feat: add i18n and l10n

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 from dev_aberto import hello
+from datetime import datetime
+from babel.dates import format_date
+import gettext
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    gettext.bindtextdomain("cli", "locale")
+    gettext.textdomain("cli")
+    _ = gettext.gettext
+
     date, name = hello()
-    print('Último commit feito em:', date, ' por', name)
+    date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%Sz")
+    print(_("Last commit made in:"), format_date(date, format="long"), _("by"), name)
